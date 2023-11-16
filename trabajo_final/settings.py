ba107c8e1493e5f182f7d0bb21f4093a71fabe44
JuanPabloTorrilla <104093280+JuanPabloTorrilla@users.noreply.github.com>
2023-11-16 18:01:34 -0300

    Update settings.py

--- trabajo_final/settings.py
+++ trabajo_final/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5t#h_0ou(paqejcm0+(2qkajw5qf#nd#))qsnceempmy0r^9d3'
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ["*"]
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 

34684d0daef20aab5ca5fb9c7ab4a5072d9196bd
JuanPabloTorrilla <104093280+JuanPabloTorrilla@users.noreply.github.com>
2023-11-16 17:43:00 -0300

    Update settings.py

--- trabajo_final/settings.py
+++ trabajo_final/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5t#h_0ou(paqejcm0+(2qkajw5qf#nd#))qsnceempmy0r^9d3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [0.0.0.0]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
